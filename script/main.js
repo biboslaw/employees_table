f4bf4632ad8428e3ed8e80753630d3a370fe88cf
seba <sebastian.pieprzyk@gmail.com>
2018-04-12 13:06:55 +0200

    v0.2

--- script/main.js
+++ script/main.js
@@ -13,7 +13,7 @@ function getData() {
             arr = JSON.parse(str);
             var txt = "";
             for (x in arr) {
-                txt += "<tr><th scope='row' data-sort='id'>" + arr[x].id + "</th><td data-sort='firstName'>" + arr[x].firstName + "</td><td data-sort='lastName'>" + arr[x].lastName + "</td><td data-sort='birthDate'>" + convertDate(arr[x].dateOfBirth) + "</td><td data-sort='company'>" + arr[x].company + "</td><td data-sort='note'>" + arr[x].note + "</td></tr>";
+                txt += "<tr><th scope='row' data-sort='id'>" + arr[x].id + "</th><td data-sort='firstName'>" + arr[x].firstName + "</td><td data-sort='lastName'>" + arr[x].lastName + "</td><td data-sort='birthDate'>" + convertDateFromJson(arr[x].dateOfBirth) + "</td><td data-sort='company'>" + arr[x].company + "</td><td data-sort='note'>" + arr[x].note + "</td></tr>";
             }
             document.querySelector("tbody").innerHTML = txt;
         },
@@ -51,49 +51,60 @@ function sortBy(e) {
     var searchCell;
     var rows, cell1, cell2;
     var count = 0;
+    var direction = "asc";
     if (sortByData == "id") {
         searchCell = "th[data-sort='" + sortByData + "']";
-    } else {
+    } 
+    else {
         searchCell = "td[data-sort='" + sortByData + "']";
     };
     while (!ifEnd) {
         rows = document.querySelectorAll("tbody tr");
-        for (var i = 0; i < rows.length-1; i++) {
-            cell1 = rows[i].querySelector(searchCell);
-            //console.log(cell1.innerHTML)
-            cell2 = rows[i + 1].querySelector(searchCell);
-            //console.log(cell2.innerHTML)
-            if (cell1.innerHTML > cell2.innerHTML) {
-                console.log(cell1.innerHTML.toLowerCase())
-                console.log(cell2.innerHTML.toLowerCase())
+        for (var i = 0; i < rows.length - 1; i++) {
+            cell1 = rows[i].querySelector(searchCell).innerHTML;
+            cell2 = rows[i + 1].querySelector(searchCell).innerHTML;
+            if (sortByData == "id" || sortByData == "note") {
+                cell1 = parseInt(cell1);
+                cell2 = parseInt(cell2);
+            } else if (sortByData == "birthDate"){
+                cell1 = convertDateToCompare(cell1);
+                console.log(cell1);
+                cell2 = convertDateToCompare(cell2);
+                console.log(cell2);
+            }
+            if (cell1 > cell2) {
                 rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
                 ifEnd = false;
-                break;   
+                break;
             }
             ifEnd = true;
         }
-        count++
-        console.log("conunt")
-        console.log(count)
     }
 }
 
-function convertDate (date){
+function convertDateFromJson(date) {
     var day, month, year;
     var zero = "0";
     var options = { year: 'numeric', month: 'long', day: 'numeric' };
     var monthsArr = ["styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec", "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"]
     date = date.slice(0, date.indexOf(" ")).replace(/\./g, "-");
     day = date.slice(0, date.indexOf("-"));
-    month = date.slice(day.length+1, date.indexOf("-", day.length+1));
+    month = date.slice(day.length + 1, date.indexOf("-", day.length + 1));
     year = date.slice(day.length + month.length + 2, date.length);
-    month = monthsArr[Number(month)-1]
-    if (day.length  == 1){
+    month = monthsArr[Number(month) - 1]
+    if (day.length == 1) {
         day = zero.concat(day);
-    } else if (month.length == 1){
+    } else if (month.length == 1) {
         month = zero.concat(month);
     }
-    //date = new Date (year, month, day);
     return day + " " + month + " " + year
-    //date.toLocaleString('en-US', options);
+}
+
+function convertDateToCompare (date) {
+    var monthsArrPL = ["styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec", "lipiec", "sierpień", "wrzesień", 
+    "październik", "listopad", "grudzień"];
+    var monthsArrUS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    var monthToConvert = date.slice(3, date.indexOf(" ", 3));
+    date = new Date (date.replace(monthToConvert, monthsArrUS[monthsArrPL.indexOf(monthToConvert)]));
+    return date;
 }
